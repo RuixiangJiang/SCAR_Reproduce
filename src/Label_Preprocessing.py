f64d73c1e73c8192579cef7a0311da65eec784b6
RuixiangJiang <2037358823@qq.com>
2025-10-02 12:42:08 -0400

    modify Label_Preprocessing.py

--- src/Label_Preprocessing.py
+++ src/Label_Preprocessing.py
@@ -1,14 +1,42 @@
 leaky_module = {
-    "train": [" s_box", "subbytes", "sb", "MixColumn"],
-    "AES_PPRM1": ["SBOX", "Mixcolumn", "MX"],
-    "AES_PPRM3": ["Sbox", "Mixcolumn", "MX"],
-    "AES_TBL": ["SBOX", "Mixcolumn", "MX"],
-    "RSA": ["MODEXP_SEQ", "MULT_BLK"],
-    "SABER": ["PMULTs"]
+    "train": [
+        ("subbytes", "s_box"),
+        ("invsubbytes", "is_box"),
+        ("subword", "s_box"),
+        ("MixColumn",),
+    ],
+    "AES_PPRM1": [
+        ("SBOX",),
+        ("Mixcolumn",),
+        ("MX",),
+    ],
+    "AES_PPRM3": [
+        ("Sbox",),
+        ("Mixcolumn",),
+        ("MX",),
+    ],
+    "AES_TBL": [
+        ("SBOX",),
+        ("Mixcolumn",),
+        ("MX",),
+    ],
+    "RSA": [
+        ("MODEXP_SEQ",),
+        ("MULT_BLK",),
+    ],
+    "SABER": [
+        ("PMULTs",),
+    ]
 }
 
 def label(Feature, design):
-    def contains_any(value, keywords):
-        return int(any(kw in str(value) for kw in keywords))
+    def contains_any(value, keyword_tuples):
+        return int(
+            any(all(kw.lower() in str(value).lower() for kw in kws) for kws in keyword_tuples)
+        )
+
     for node in Feature.keys():
-        Feature[node]["label"] = contains_any(Feature[node]["Node"], leaky_module.get(design, []))
+        Feature[node]["label"] = contains_any(
+            Feature[node]["Node"],
+            leaky_module.get(design, [])
+        )
